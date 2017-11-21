d1c7e66a4dbcb755cdb4e6e608010ecc266a16e8
canpan14 <sammachlin@hotmail.com>
2017-11-21 15:48:09 -0500

    Add a couple more cards to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 # cards table
 cards_to_create = [
   { name: 'Swordsman', cost: 5, card_type: 'attack', attack: 2, defense: 1, description: 'Basic stabby guy', spell_effect: '' },
-  { name: 'Trebuchet', cost: 23, card_type: 'attack', attack: 5, defense: 0, description: 'Can launch a 90kg stone over 300m', spell_effect: '' },
-  { name: 'Pikeman', cost: 14, card_type: 'defense', attack: 1, defense: 3, description: 'Will not do much harm if you do not run into them', spell_effect: '' }
+  { name: 'Assassin', cost: 11, card_type: 'attack', attack: 4, defense: 0, description: 'Sneaky stabby guy', spell_effect: '' },
+  { name: 'Flaming Pigs', cost: 1, card_type: 'attack', attack: 1, defense: 0, description: 'Oink Oink', spell_effect: '' },
+  { name: 'Trebuchet', cost: 23, card_type: 'defense', attack: 2, defense: 4, description: 'Can launch a 90kg stone over 300m', spell_effect: '' },
+  { name: 'Pikeman', cost: 14, card_type: 'defense', attack: 1, defense: 3, description: 'Harmless if you do not run into them', spell_effect: '' },
+  { name: 'Archer', cost: 9, card_type: 'defense', attack: 3, defense: 1, description: 'Pointy object shooter', spell_effect: '' }
 ]
 
 cards_to_create.each do |card|

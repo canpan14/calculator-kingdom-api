c4ae4e98485bc18161b4207f00641b200f9a311f
canpan14 <sammachlin@hotmail.com>
2017-11-28 21:56:58 -0500

    Add a couple more card types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ cards_to_create = [
   { name: 'Flaming Pigs', cost: 1, card_type: 'attack', attack: 1, defense: 0, description: 'Oink Oink', spell_effect: '' },
   { name: 'Trebuchet', cost: 23, card_type: 'defense', attack: 2, defense: 4, description: 'Can launch a 90kg stone over 300m', spell_effect: '' },
   { name: 'Pikeman', cost: 14, card_type: 'defense', attack: 1, defense: 3, description: 'Harmless if you do not run into them', spell_effect: '' },
-  { name: 'Archer', cost: 9, card_type: 'defense', attack: 3, defense: 1, description: 'Pointy object shooter', spell_effect: '' }
+  { name: 'Archer', cost: 9, card_type: 'defense', attack: 3, defense: 1, description: 'Pointy object shooter', spell_effect: '' },
+  { name: 'Boiling Oil', cost: 36, card_type: 'defense', attack: 0, defense: 7, description: 'Nothing wrong with deep fried people for breakfast', spell_effect: '' },
+  { name: 'Village Idiot', cost: 18, card_type: 'attack', attack: 4, defense: 1, description: 'Death by annoyance, an underrated attack tactic', spell_effect: '' }
 ]
 
 cards_to_create.each do |card|
